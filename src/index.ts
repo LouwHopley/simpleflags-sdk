5265e31b65bbb0390c8f2738f1e223208b9a46fa
Louw Hopley <louw@root.co.za>
2024-07-20 14:49:55 +0200

    Minor updates

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,23 @@
-let fetchFunction: any;
+import fetch from 'node-fetch';
 
+let fetchFunction: typeof fetch | any;
+
+// Check if we're in a browser environment
 if (typeof window !== 'undefined' && window.fetch) {
-  // In a browser environment
   fetchFunction = window.fetch.bind(window);
 } else {
-  // In a Node.js environment
-  const https = require('https');
-  fetchFunction = (url: string, options?: RequestInit) => {
-    return new Promise<Response>((resolve, reject) => {
-      const parsedUrl = new URL(url);
-      const requestOptions = {
-        method: options?.method || 'GET',
-        headers: options?.headers,
-      };
-
-      const req = https.request(parsedUrl, requestOptions, (res: any) => {
-        let data = '';
-
-        res.on('data', (chunk: any) => {
-          data += chunk;
-        });
-
-        res.on('end', () => {
-          const response = new Response(data, {
-            status: res.statusCode,
-            statusText: res.statusMessage,
-            headers: res.headers as unknown as HeadersInit,
-          });
-          resolve(response);
-        });
-      });
-
-      req.on('error', reject);
-
-      if (options?.body) {
-        req.write(options.body);
-      }
-
-      req.end();
-    });
-  };
+  fetchFunction = fetch;
 }
+const baseUrl = 'https://simpleflags.io/api/flag';
 
-class SimpleFlags {
-  static baseUrl = 'https://simpleflags.io/api/flag';
+const isEnabled = async (props: { teamId: string; environmentKey: string; flagKey: string; groupingKey: string }): Promise<boolean> => {
+  const { teamId, environmentKey, flagKey, groupingKey } = props;
+  const url = `${baseUrl}/${flagKey}/${environmentKey}/${groupingKey}`;
+  const response = await fetchFunction(url);
+  const data = await response.json() as any;
+  return data.status === 'enabled';
+};
 
-  static async isEnabled(props: { teamId: string, environmentKey: string, flagKey: string, groupingKey: string }): Promise<boolean> {
-    const { teamId, environmentKey, flagKey, groupingKey } = props;
-    const url = `${this.baseUrl}/${flagKey}/${environmentKey}/${groupingKey}`;
-    const response = await fetchFunction(url);
-    const data = await response.json();
-    return data.status === 'enabled';
-  }
-}
-
-export default SimpleFlags;
+export default {
+  isEnabled,
+};
